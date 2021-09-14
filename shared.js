a5c30c81e864539f7a4db92c70d0a10ca62dc5e2
Yuhan <yzho0151@student.monash.edu>
2021-09-14 19:27:35 +1000

    Wrote the attributes to the searchResult class

---
+++ shared.js
@@ -0,0 +1,14 @@
+// Constructing Classes
+
+class SearchResult {
+    constructor(name, lat, lng, address, category, bookmarked, review){
+        this.name = name;
+        this.lat = lat;
+        this.lng = lng;
+        this.address = address;
+        this.category = category;
+        this.bookmarked = bookmarked = false;
+        this.review = review; 
+        
+    }
+}

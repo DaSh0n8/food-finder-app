14fa0d77c682b0ef73001c06dc544c16476fb5da
Adam Yang <ayan0024@student.monash.edu>
2021-09-15 19:20:20 +1000

    Centrepoint class and local storage

---
+++ shared.js
@@ -0,0 +1,161 @@
+"use strict";
+
+//Local Storage Keys
+const CENTREPOINT_LIST_KEY = 'centrepointList';
+
+
+
+//CLASSES
+
+//Centrepoint class
+class Centrepoint
+{
+    constructor(lat, lng, address)
+    {
+        this._lat = lat;
+        this._lng = lng;
+        this._address = address;
+        this._bookmarked = false;
+    }
+
+    get lat()
+    {
+        return this._lat;
+    }
+
+    get lng()
+    {
+        return this._lng;
+    }
+
+    get address()
+    {
+        return this._address;
+    }
+
+    get bookmarked()
+    {
+        return this._bookmarked;
+    }
+
+    set bookmarked(bookmarked)
+    {
+        this._bookmarked = bookmarked;
+    }
+
+    fromData(dataObject)
+    {
+        this._lat = dataObject._lat;
+        this._lng = dataObject._lng;
+        this._address = dataObject._address;
+        this._bookmarked = dataObject._bookmarked;
+    }
+}
+
+//Centrepoint List Class
+class CentrepointList
+{
+    constructor()
+    {
+        this._list = [];
+    }
+
+    get list()
+    {
+        return this._list;
+    }
+
+    addCentrepoint(centrepointInstance)
+    {
+        this._list.push(centrepointInstance);
+    }
+
+    fromData(dataObject)
+    {
+        for (let i = 0; i < dataObject._list.length; i++)
+        {
+            let centrepointInstance = new Centrepoint;
+            centrepointInstance.fromData(dataObject._list[i]);
+            this.addCentrepoint(centrepointInstance);
+        }
+    }
+}
+
+
+
+//LOCAL STORAGE FUNCTIONS
+
+//check if local storage is available
+function checkLocalStorage(key)
+{
+    if (typeof (Storage) !== "undefined")
+    {
+        if (localStorage.getItem(key) !== null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else
+    {
+        return false;
+    }
+}
+
+//set local storage
+function setLocalStorage(key, data)
+{
+    data = JSON.stringify(data);
+    localStorage.setItem(key, data);
+}
+
+//get data
+function getLocalStorage(key)
+{
+    let data = localStorage.getItem(key);
+    try
+    {
+        data = JSON.parse(data);
+    }
+    catch (e)
+    {
+        console.log(e);
+    }
+    finally
+    {
+        return data;
+    }
+}
+
+
+
+//Initialises centrepoint list if none exists
+let centrepointList = new CentrepointList();
+if (typeof Storage !== 'undefined')
+{
+    if (checkLocalStorage(CENTREPOINT_LIST_KEY) == true)
+    {
+        let centrepointListData = getLocalStorage(CENTREPOINT_LIST_KEY);
+        centrepointList.fromData(centrepointListData);
+    }
+    else
+    {
+        setLocalStorage(CENTREPOINT_LIST_KEY, centrepointList);
+    }
+}
+
+
+
+//centrepoint test function
+function testCentrepoints()
+{
+    let test = new Centrepoint(1, 2 ,'d');
+    centrepointList.addCentrepoint(test);
+    test = new Centrepoint(3, 4, '2');
+    centrepointList.addCentrepoint(test);
+    test = new Centrepoint(8, 5, 'my house');
+    centrepointList.addCentrepoint(test);
+}
